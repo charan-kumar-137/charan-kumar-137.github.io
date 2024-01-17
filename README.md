f8e358503570e21f903b1b42a25140e50e2e3781
Charan Kumar N <charankumar137@gmail.com>
2024-01-17 21:10:47 +0530

    Updated Code to use Vue

---
+++ README.md
@@ -0,0 +1,1 @@
+# charan-kumar-137.github.io
---
+++ src/components/Banner.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="home_banner_area">
+        <div class="banner_inner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3">
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="banner_content home_left_img">
+                            <h5>Hi there! I'm</h5>
+                            <h2>Charan Kumar N</h2>
+                            <h5>Software Engineer</h5>
+                            <h4>
+                                <a class="social-web-banner-m" href="https://github.com/charan-kumar-137" target="_blank"><i class="fab fa-github social-web-banner"></i></a>
+                                <a class="social-web-banner-m" href="https://linkedin.com/in/--charan-kumar-n--" target="_blank"><i class="fab fa-linkedin social-web-banner"></i></a>
+                                <a class="email social-web-banner-m" href="mailto:charankumar1331@gmail.com" target="_blank"><i class="fas fa-envelope social-web-banner"></i></a>
+                            </h4>
+
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'AppBanner',
+    }
+</script>
